99745064ffc9b358a8b598ebce55c3273d626e9d
m3ryck <adriano.bsanto@eng.ci.ufpb.br>
2020-03-16 10:03:31 -0300

    Adicionando Arquivos

---
+++ src/componentes/DoisComponentes.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const CompA = Props =>
+    <h1>Primeiro diz: {Props.valor}</h1>
+
+const CompB = Props =>
+    <h1>Segundo diz: {Props.valor}</h1>
+
+export {CompA,CompB}
+    
