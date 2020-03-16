859e59dee77ef0f80a9fd1428cf2df5e24950126
m3ryck <adriano.bsanto@eng.ci.ufpb.br>
2020-03-16 12:37:51 -0300

    Aula 19

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 // import PrimeiroComponente from './componentes/PrimeiroComponente'
-import {CompA,CompB as B} from './componentes/DoisComponentes'
+// import {CompA,CompB as B} from './componentes/DoisComponentes'
 // import MultiElementos from './componentes/MultiElementos'
-import FamiliaSilva from './componentes/FamiliaSilva'
-
+// import FamiliaSilva from './componentes/FamiliaSilva'
+import Familia from './componentes/Familia'
+import Membro from './componentes/Membro'
 
 const elemento = document.getElementById('root')
 // ReactDOM.render(<h1>ola react</h1>,elemento)
 
 ReactDOM.render(
     <div>
-        <FamiliaSilva/>
+        <Familia  sobrenome ='Pereira'>
+            <Membro nome = 'Andre'/>
+            <Membro nome = 'Mariana'/>
+        </Familia>
+
+        {/* <Familia>
+            <Membro nome = 'Adriano' sobrenome ='brito'/>
+            <Membro nome = 'Mari' sobrenome = 'brito'/>
+        </Familia> */}
         {/* <MultiElementos/> */}
         {/* <CompA valor = "olá eu sou A!!" /> }
         /* <B valor = "olá eu sou B!!" />  */}
